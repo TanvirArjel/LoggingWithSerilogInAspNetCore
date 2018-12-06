f719904b9aac81165930bd567d694b6cb9f2bbf0
Tanvir Ahmad Arjel <tanvirahmad.du@gmail.com>
2018-12-06 23:20:25 +0600

    Initial Commit

---
+++ LoggingWithSerilogInAspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using LoggingWithSerilogInAspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace LoggingWithSerilogInAspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+            try
+            {
+                Convert.ToInt16("He");
+            }
+            catch (Exception exception)
+            {
+               _logger.LogError(exception,"Exception thrown!");
+            }
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoggingWithSerilogInAspNetCore/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System;
+using System.IO;
+
+namespace LoggingWithSerilogInAspNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information() // Set the minimun log level
+                .WriteTo.File("Logs\\log-.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit:7) // this is for logging into file system
+                .WriteTo.MSSqlServer(connectionString : connectionString,tableName: "LogTable",
+                columnOptions: new ColumnOptions(), autoCreateSqlTable: true) // this is for logging into database
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging => { logging.ClearProviders(); }) // clearing all other logging providers
+                .UseSerilog(); // Using serilog 
+    }
+}
